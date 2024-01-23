fb159670bd8acb9c44389773c331589d325eb227
Alex H <48185123+Vodes@users.noreply.github.com>
2024-01-23 03:22:45 +0100

    Theme switch animation expanded

--- src/main/kotlin/moe/styx/theme/Theme.kt
+++ src/main/kotlin/moe/styx/theme/Theme.kt
@@ -1,6 +1,7 @@
 package moe.styx.theme
 
 import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.TweenSpec
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ColorScheme
@@ -63,27 +64,41 @@ val AppTypography = Typography(
     labelSmall = defaultTypo.labelSmall.copy(fontFamily = AppFont.OpenSans)
 )
 
+
+private val animationSpec: TweenSpec<Color> = tween(durationMillis = 650)
+
 @Composable
-private fun animateColor(targetValue: Color) =
-    animateColorAsState(
-        targetValue = targetValue,
-        animationSpec = tween(durationMillis = 650)
-    ).value
+private fun animateColor(targetValue: Color) = animateColorAsState(targetValue = targetValue, animationSpec = animationSpec).value
 
 @Composable
 fun ColorScheme.transition() = copy(
     primary = animateColor(primary),
-    primaryContainer = animateColor(primaryContainer),
     onPrimary = animateColor(onPrimary),
+    primaryContainer = animateColor(primaryContainer),
     onPrimaryContainer = animateColor(onPrimaryContainer),
     secondary = animateColor(secondary),
     onSecondary = animateColor(onSecondary),
     secondaryContainer = animateColor(secondaryContainer),
     onSecondaryContainer = animateColor(onSecondaryContainer),
     tertiary = animateColor(tertiary),
+    onTertiary = animateColor(onTertiary),
+    tertiaryContainer = animateColor(tertiaryContainer),
+    onTertiaryContainer = animateColor(onTertiaryContainer),
+    error = animateColor(error),
+    errorContainer = animateColor(errorContainer),
+    onError = animateColor(onError),
+    onErrorContainer = animateColor(onErrorContainer),
     background = animateColor(background),
     onBackground = animateColor(onBackground),
     surface = animateColor(surface),
     onSurface = animateColor(onSurface),
+    surfaceVariant = animateColor(surfaceVariant),
+    onSurfaceVariant = animateColor(onSurfaceVariant),
     outline = animateColor(outline),
+    inverseOnSurface = animateColor(inverseOnSurface),
+    inverseSurface = animateColor(inverseSurface),
+    inversePrimary = animateColor(inversePrimary),
+    surfaceTint = animateColor(surfaceTint),
+    outlineVariant = animateColor(outlineVariant),
+    scrim = animateColor(scrim)
 )
