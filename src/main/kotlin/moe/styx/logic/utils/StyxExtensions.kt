297f899f0b1b47601e5b810b21d3531d04b6a41d
Alex H <48185123+Vodes@users.noreply.github.com>
2024-01-31 22:38:25 +0100

    Make the Mapping Icons actually do something

--- src/main/kotlin/moe/styx/logic/utils/StyxExtensions.kt
+++ src/main/kotlin/moe/styx/logic/utils/StyxExtensions.kt
@@ -39,3 +39,23 @@ fun Media.favAdded(): Long {
     val fav = DataManager.favourites.value.find { it.mediaID.equals(GUID, true) }
     return fav?.added ?: 0L
 }
+
+enum class StackType {
+    ANILIST,
+    TMDB,
+    MAL
+}
+
+fun Media.getURLFromMap(type: StackType): String? {
+    val mappings = runCatching {
+        json.decodeFromString<MappingCollection>(this.metadataMap!!)
+    }.getOrNull() ?: return null
+    return when (type) {
+        StackType.TMDB -> {
+            mappings.tmdbMappings.minByOrNull { it.remoteID }?.remoteID?.let { "https://themoviedb.org/${if (this.isSeries.toBoolean()) "tv" else "movie"}/$it" }
+        }
+
+        StackType.MAL -> mappings.malMappings.minByOrNull { it.remoteID }?.remoteID?.let { "https://myanimelist.net/anime/$it" }
+        else -> mappings.anilistMappings.minByOrNull { it.remoteID }?.remoteID?.let { "https://anilist.co/anime/$it" }
+    }
+}
--- src/main/kotlin/moe/styx/views/anime/AnimeDetailView.kt
+++ src/main/kotlin/moe/styx/views/anime/AnimeDetailView.kt
@@ -1,19 +1,19 @@
 package moe.styx.views.anime
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.selection.SelectionContainer
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.FilterQuality
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.model.ScreenModel
@@ -26,10 +26,15 @@ import moe.styx.components.MainScaffold
 import moe.styx.components.anime.*
 import moe.styx.components.misc.FavouriteIconButton
 import moe.styx.logic.data.*
+import moe.styx.logic.utils.StackType
+import moe.styx.logic.utils.getURLFromMap
 import moe.styx.navigation.LocalGlobalNavigator
 import moe.styx.settings
+import moe.styx.theme.AppShapes
 import moe.styx.types.Media
 import moe.styx.types.MediaEntry
+import java.awt.Desktop
+import java.net.URI
 import java.util.*
 
 class AnimeDetailView(val ID: String) : Screen {
@@ -118,18 +123,7 @@ fun StupidImageNameArea(media: Media) {
             Column(Modifier.fillMaxWidth().weight(1f, true)) {
                 MediaNameListing(media, Modifier.align(Alignment.Start))//, Modifier.weight(0.5F))
                 Spacer(Modifier.weight(1f, true))
-                Row(verticalAlignment = Alignment.CenterVertically) {
-                    val filter = ColorFilter.tint(MaterialTheme.colorScheme.onSurface)
-                    IconButton({ println("Test") }, Modifier.padding(7.dp, 15.dp).size(25.dp)) {
-                        Image(painterResource("icons/al.svg"), "Anilist", colorFilter = filter)
-                    }
-                    IconButton({ println("Test") }, Modifier.padding(7.dp, 15.dp).size(35.dp)) {
-                        Image(painterResource("icons/myanimelist.svg"), "MyAnimeList", colorFilter = filter)
-                    }
-                    IconButton({ println("Test") }, Modifier.padding(7.dp, 15.dp).size(25.dp)) {
-                        Image(painterResource("icons/tmdb.svg"), "TheMovieDB", colorFilter = filter)
-                    }
-                }
+                MappingIcons(media)
             }
         }
     }
@@ -145,5 +139,48 @@ class AnimeDetailViewModel(val ID: String) : ScreenModel {
 
         episodes = DataManager.entries.value.filter { it.mediaID == anime!!.GUID }.sortedByDescending { it.entryNumber.toDoubleOrNull() ?: 0.0 }
         return episodes
+    }
+}
+
+@Composable
+fun MappingIcons(media: Media) {
+    val malURL = media.getURLFromMap(StackType.MAL)
+    val anilistURL = media.getURLFromMap(StackType.ANILIST)
+    val tmdbURL = media.getURLFromMap(StackType.TMDB)
+    Row(Modifier.padding(0.dp, 0.dp, 0.dp, 15.dp), verticalAlignment = Alignment.CenterVertically) {
+        val filter = ColorFilter.tint(MaterialTheme.colorScheme.onSurface)
+        if (!anilistURL.isNullOrBlank())
+            Image(
+                painterResource("icons/al.svg"),
+                "AniList",
+                Modifier.padding(8.dp, 3.dp).size(25.dp).clip(AppShapes.small).clickable {
+                    if (Desktop.isDesktopSupported())
+                        Desktop.getDesktop().browse(URI(anilistURL))
+                },
+                contentScale = ContentScale.FillWidth,
+                colorFilter = filter
+            )
+        if (!malURL.isNullOrBlank())
+            Image(
+                painterResource("icons/myanimelist.svg"),
+                "MyAnimeList",
+                Modifier.padding(8.dp, 3.dp).size(25.dp).clip(AppShapes.small).clickable {
+                    if (Desktop.isDesktopSupported())
+                        Desktop.getDesktop().browse(URI(malURL))
+                },
+                contentScale = ContentScale.FillWidth,
+                colorFilter = filter
+            )
+        if (!tmdbURL.isNullOrBlank())
+            Image(
+                painterResource("icons/tmdb.svg"),
+                "TheMovieDB",
+                Modifier.padding(8.dp, 3.dp).size(25.dp).clip(AppShapes.small).clickable {
+                    if (Desktop.isDesktopSupported())
+                        Desktop.getDesktop().browse(URI(tmdbURL))
+                },
+                contentScale = ContentScale.FillWidth,
+                colorFilter = filter
+            )
     }
 }
