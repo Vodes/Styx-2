443bcd575ad67d49e916573794aec502700f7211
Alex H <48185123+Vodes@users.noreply.github.com>
2024-03-30 01:14:29 +0100

    feat: Movie downloads
    Copypasted from the mobile app

--- src/main/kotlin/moe/styx/views/anime/MovieDetailView.kt
+++ src/main/kotlin/moe/styx/views/anime/MovieDetailView.kt
@@ -11,24 +11,28 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.core.screen.ScreenKey
 import com.russhwolf.settings.get
 import moe.styx.common.compose.components.anime.*
 import moe.styx.common.compose.components.buttons.FavouriteIconButton
+import moe.styx.common.compose.components.buttons.IconButtonWithTooltip
 import moe.styx.common.compose.components.layout.MainScaffold
 import moe.styx.common.compose.components.misc.OnlineUsersIcon
-import moe.styx.common.compose.files.Storage
-import moe.styx.common.compose.files.collectWithEmptyInitial
-import moe.styx.common.compose.files.getCurrentAndCollectFlow
+import moe.styx.common.compose.files.*
 import moe.styx.common.compose.http.login
 import moe.styx.common.compose.settings
+import moe.styx.common.compose.threads.DownloadQueue
 import moe.styx.common.compose.threads.RequestQueue
 import moe.styx.common.compose.utils.LocalGlobalNavigator
+import moe.styx.common.data.MediaEntry
 import moe.styx.common.data.MediaWatched
 import moe.styx.common.extension.currentUnixSeconds
 import moe.styx.common.extension.eqI
 import moe.styx.common.extension.toBoolean
+import moe.styx.common.util.SYSTEMFILES
+import moe.styx.common.util.launchThreaded
 import moe.styx.components.anime.AppendDialog
 import moe.styx.components.anime.FailedDialog
 import moe.styx.logic.runner.currentPlayer
@@ -120,6 +124,8 @@ class MovieDetailView(private val mediaID: String) : Screen {
                                 Spacer(Modifier.weight(1f))
                                 Text(movieEntry?.fileSize?.readableSize() ?: "", style = MaterialTheme.typography.bodyMedium)
                             }
+                            if (movieEntry != null)
+                                DownloadRow(movieEntry)
                             if (watched != null) {
                                 WatchedIndicator(watched, Modifier.fillMaxWidth().padding(0.dp, 2.dp, 0.dp, 5.dp))
                             }
@@ -139,6 +145,56 @@ class MovieDetailView(private val mediaID: String) : Screen {
                     if (media.sequel != null || media.prequel != null) {
                         HorizontalDivider(Modifier.fillMaxWidth().padding(8.dp, 6.dp), thickness = 2.dp)
                         MediaRelations(media, mediaList) { nav.pushMediaView(it, true) }
+                    }
+                }
+            }
+        }
+    }
+
+    @Composable
+    private fun DownloadRow(entry: MediaEntry) {
+        val downloaded by Storage.stores.downloadedStore.collectWithEmptyInitial()
+        val currentlyDownloading by DownloadQueue.currentDownload.collectAsState()
+        val queued by DownloadQueue.queuedEntries.collectAsState()
+        Row(Modifier.padding(8.dp, 5.dp).fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
+            val isDownloaded = downloaded.find { it.entryID eqI entry.GUID } != null
+            val isQueued = queued.contains(entry.GUID)
+            val progress = currentlyDownloading?.let { if (it.entryID eqI entry.GUID) it else null }
+            if (isQueued || progress != null) {
+                if (isQueued) {
+                    Icon(Icons.Default.Downloading, "Queued", modifier = Modifier.size(20.dp))
+                } else if (progress != null) {
+                    Box {
+                        Icon(Icons.Default.Download, "Downloading", modifier = Modifier.size(14.dp).zIndex(1F).align(Alignment.Center))
+                        CircularProgressIndicator(
+                            { progress.progressPercent.toFloat() / 100 },
+                            modifier = Modifier.size(23.dp).zIndex(2F).align(Alignment.Center),
+                            trackColor = MaterialTheme.colorScheme.onSurface.copy(0.4F),
+                            color = MaterialTheme.colorScheme.primary,
+                            strokeWidth = 3.dp
+                        )
+                    }
+                }
+            }
+            if (progress == null) {
+                if (isDownloaded || isQueued) {
+                    IconButtonWithTooltip(Icons.Default.Delete, "Delete") {
+                        if (queued.contains(entry.GUID)) {
+                            launchThreaded { DownloadQueue.queuedEntries.emit(queued.toMutableList().filterNot { it eqI entry.GUID }.toList()) }
+                        }
+                        val downloadedEntry = downloaded.find { it.entryID eqI entry.GUID }
+                        if (downloadedEntry != null) {
+                            SYSTEMFILES.delete(downloadedEntry.okioPath)
+                        }
+                        launchThreaded {
+                            Storage.stores.downloadedStore.updateList { list ->
+                                list.removeAll { it.entryID eqI entry.GUID }
+                            }
+                        }
+                    }
+                } else {
+                    IconButtonWithTooltip(Icons.Default.DownloadForOffline, "Download") {
+                        DownloadQueue.addToQueue(entry)
                     }
                 }
             }
