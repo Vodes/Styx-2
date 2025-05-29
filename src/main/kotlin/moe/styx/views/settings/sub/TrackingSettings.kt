ea7a1c91c632abee4e7c1a985704d7cb8e85ca87
Alex H. <48185123+Vodes@users.noreply.github.com>
2025-05-30 01:33:44 +0200

    feat: Add tracking settings for MAL and a refresh button

--- src/main/kotlin/moe/styx/views/settings/sub/TrackingSettings.kt
+++ src/main/kotlin/moe/styx/views/settings/sub/TrackingSettings.kt
@@ -1,9 +1,10 @@
 package moe.styx.views.settings.sub
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.OpenInBrowser
+import androidx.compose.material.icons.outlined.Refresh
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -11,17 +12,19 @@ import cafe.adriel.voyager.core.model.rememberNavigatorScreenModel
 import com.russhwolf.settings.get
 import com.russhwolf.settings.set
 import moe.styx.Styx_2.BuildConfig
+import moe.styx.common.compose.components.AppShapes
 import moe.styx.common.compose.components.misc.Toggles
+import moe.styx.common.compose.components.tracking.common.TrackingConnectionIndicator
 import moe.styx.common.compose.settings
 import moe.styx.common.compose.utils.LocalGlobalNavigator
+import moe.styx.common.compose.utils.openURI
 import moe.styx.common.compose.viewmodels.MainDataViewModel
-import moe.styx.logic.runner.openURI
 
 @Composable
 fun TrackingSettings() {
     val nav = LocalGlobalNavigator.current
     val sm = nav.rememberNavigatorScreenModel("main-vm") { MainDataViewModel() }
-    Column(Modifier.fillMaxWidth().height(IntrinsicSize.Max)) {
+    Column(Modifier.fillMaxWidth()) {
         Toggles.ContainerSwitch(
             "Auto-Sync",
             "Automatically update remote list status after watching something.",
@@ -30,21 +33,33 @@ fun TrackingSettings() {
             settings["auto-sync"] = it
         }
         Column(Modifier.fillMaxWidth().padding(12.dp, 5.dp)) {
-            if (sm.anilistUser == null) {
-                Text("AniList not connected!", style = MaterialTheme.typography.titleMedium)
-            } else {
-                Text("AniList user connected: ${sm.anilistUser?.name}", style = MaterialTheme.typography.titleMedium)
+            Row {
+                TrackingConnectionIndicator(username = sm.anilistUser?.name, siteName = "AniList", userBaseURL = "https://anilist.co/user")
+                TrackingConnectionIndicator(username = sm.malUser?.name, siteName = "MyAnimeList", userBaseURL = "https://myanimelist.net/profile")
             }
             Spacer(Modifier.height(4.dp))
             Text(
-                "To dis-/connect an account, please go to the website and check the anilist tab in the user section.\nYou will have to restart this app after doing so.",
+                "To dis-/connect an account, please go to the website and check the tabs in the user section.",
                 style = MaterialTheme.typography.bodyMedium,
                 modifier = Modifier.padding(6.dp, 4.dp)
             )
-            Button({
-                openURI("${BuildConfig.SITE_URL}/user")
-            }) {
-                Text("Open ${BuildConfig.SITE}")
+            Row(Modifier.padding(0.dp, 2.dp)) {
+                Button({
+                    openURI("${BuildConfig.SITE_URL}/user")
+                }, shape = AppShapes.medium, modifier = Modifier.padding(6.dp, 0.dp)) {
+                    Icon(Icons.Outlined.OpenInBrowser, "Open styx website")
+                    Spacer(Modifier.width(5.dp))
+                    Text("Open ${BuildConfig.SITE}")
+                }
+                Button(
+                    { sm.reauthorizeStyx() },
+                    shape = AppShapes.medium,
+                    colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.secondary),
+                ) {
+                    Icon(Icons.Outlined.Refresh, "Refresh tracking connections")
+                    Spacer(Modifier.width(2.dp))
+                    Text("Refresh")
+                }
             }
         }
         Spacer(Modifier.height(2.dp))
