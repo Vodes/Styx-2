848e2bd1a3c2670dec60baf46d457a28194d9391
Alex H. <48185123+Vodes@users.noreply.github.com>
2025-06-20 02:50:26 +0200

    chore: Bump to new release version

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,11 +13,11 @@ plugins {
 }
 
 group = "moe.styx"
-version = "0.2.0-rc2"
+version = "0.2.0"
 
 // Necessary to have working Windows installers for rc/beta/etc versions.
 // Count up by one for every release until a new MINOR version bump.
-val subVersionClassifier = 1
+val subVersionClassifier = 2
 
 repositories {
     google()
