9ddb7133a395c0a4e18d6b397dfb820a08dab84d
Alex H. <48185123+Vodes@users.noreply.github.com>
2026-05-20 01:25:35 +0200

    chore: Add local build for common-compose

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,3 +10,12 @@ plugins {
 }
 rootProject.name = "Styx 2"
 
+val localCommonCompose = file("../Styx-Common-Compose")
+if (localCommonCompose.isDirectory) {
+    includeBuild(localCommonCompose) {
+        dependencySubstitution {
+            substitute(module("moe.styx:styx-common-compose-jvm"))
+                .using(project(":styx-common-compose"))
+        }
+    }
+}
